406b74608ce1d4471214f8bc14a043c6d9f46603
RavL84 <raulsistemas06@gmail.com>
2022-11-16 22:59:41 -0300

    quarto-commit

---
+++ App/Control/ElementControlTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use Livro\Control\AbstractPage;
+use Livro\Widgets\Base\Element;
+
+/**
+ * Description of ElementControlTest
+ * 
+ * Essa classe axilia na montagem de tags de forma automatica, configura estilos
+ *   e exibe o resultado montado na tela.
+ * 
+ * Essa classe assim como as demais de controle extende a AbstractPage que analisa
+ * a requisição e executa a classe de controle.
+ *
+ * @author raul
+ * @date 16/11/2022
+ * 
+ */
+class ElementControlTest  extends AbstractPage{
+    public function __construct() {
+        
+        parent::__construct();
+        
+       $div = new Element('div');
+        $div->style = 'text-align:center;';
+        $div->style.= 'font-weight:bold;';
+        $div->style.= 'font-size:14pt;';
+        $div->style.= 'margin:20px;';
+        
+        $p = new Element('p');
+        $p->add('Isto é um teste de parágrafo');
+        
+        $div->add($p);
+       
+        
+        parent::add($div);
+        
+    }
+}
--- Lib/Livro/Control/AbstractPage.php
+++ Lib/Livro/Control/AbstractPage.php
@@ -1,5 +1,8 @@
 <?php
+
 namespace Livro\Control;
+
+use Livro\Widgets\Base\Element;
 
 /**
  * Description of AbstractPage
@@ -9,22 +12,24 @@ namespace Livro\Control;
  * @author raul
  * @date 16/11/2022
  */
+class AbstractPage extends Element {
 
-class AbstractPage 
-{    
+    public function __construct($name) {
+        parent::__construct('div');
+    }
+
     /**
      * Executa determinado método de acordo com os parâmetros recebidos
      */
-    public function show()
-    {
-        if ($_GET)
-        {
+    public function show() {
+        if ($_GET) {
             $method = isset($_GET['method']) ? $_GET['method'] : null;
-            
-            if (method_exists($this, $method))
-            {
-                call_user_func( [$this, $method], $_REQUEST);
+
+            if (method_exists($this, $method)) {
+                call_user_func([$this, $method], $_REQUEST);
             }
         }
+        parent::show();
     }
+
 }
